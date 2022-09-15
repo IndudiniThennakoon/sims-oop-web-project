1158fbbc1e76e5e3fd26547272545c2d043d672d
Prashantha K.G.M <it21169908@my.sliit.lk>
2022-09-15 20:19:30 +0530

    Add database connection configs and environment variables

--- src/main/java/com/sims/configs/Config.java
+++ src/main/java/com/sims/configs/Config.java
@@ -3,7 +3,9 @@
  */
 package com.sims.configs;
 
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author maneesh
@@ -11,6 +13,53 @@ package com.sims.configs;
  */
 public class Config {
 	
+	private static final Properties prop = new Properties();
 	
+    private static String MYSQL_URL = "";
+    private static String DB_NAME = "";
+    private static String DB_USERNAME = "";
+    private static String DB_PASSWORD = "";
+    private static String DRIVER_NAME = "";
+
+	static {
+		//Static Initialization Block : In the entire program, this will execute only one time.
+		try {
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			InputStream input = classLoader.getResourceAsStream("WEB-INF/environments/env.properties");
+
+//        prop.load(new FileInputStream("env.properties"));
+	        prop.load(input);
+
+	        MYSQL_URL = prop.getProperty("MYSQL_URL");
+	        DB_NAME = prop.getProperty("DB_NAME");
+	        DB_USERNAME = prop.getProperty("DB_USERNAME");
+	        DB_PASSWORD = prop.getProperty("DB_PASSWORD");
+	        DRIVER_NAME = prop.getProperty("DRIVER_NAME");
+
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+    
+    
+    public static String getMySqlUrl() {
+        return MYSQL_URL;
+    }
+    
+    public static String getDbName() {
+        return DB_NAME;
+    }
+    
+    public static String getDbUserName() {
+        return DB_USERNAME;
+    }
+    
+    public static String getDbPassword() {
+        return DB_PASSWORD;
+    }
+    
+    public static String getDriverName() {
+        return DRIVER_NAME;
+    }
     
 }
--- src/main/java/com/sims/configs/ConnectionProvider.java
+++ src/main/java/com/sims/configs/ConnectionProvider.java
@@ -3,12 +3,33 @@
  */
 package com.sims.configs;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author maneesh
  *
  */
-public class ConnectionProvider {
+public class ConnectionProvider extends Config {
     
-
+    public static Connection getConnection() {
+        
+        String mysqlUrl = getMySqlUrl();
+        String dbName = getDbName();
+        String dbUserName = getDbUserName();
+        String dbPassword = getDbPassword();
+        String driverName = getDriverName();
+        
+        try {
+            Class.forName(driverName);
+            Connection con = DriverManager.getConnection(mysqlUrl + "/" +dbName, dbUserName, dbPassword);
+            return con;
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("SQLException -" + e.getMessage());
+            return null;
+        }
+    }
     
 }
