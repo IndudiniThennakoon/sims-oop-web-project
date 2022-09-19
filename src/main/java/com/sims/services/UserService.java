d1655a48fc83f292a99983f3fceecfac8e74796d
Prashantha K.G.M <it21169908@my.sliit.lk>
2022-09-20 03:23:00 +0530

    UserService and UserServiceInterface created

--- src/main/java/com/sims/services/UserService.java
+++ src/main/java/com/sims/services/UserService.java
@@ -3,12 +3,43 @@
  */
 package com.sims.services;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import com.sims.configs.ConnectionProvider;
+import com.sims.models.User;
+import com.sims.services.interfaces.UserServiceInterface;
+import com.sims.utils.QueryBuilder;
+import com.sims.utils.modelHelper;
+
 /**
  * This is the User Service class
  * 
  * @author maneesh
  */
+public class UserService implements UserServiceInterface {
 
-public class UserService {
+	@Override
+	public User getUserByEmail(String email) {
+		
+		User user = null;
+		Connection con = null;
+		
+		try {
+	        con = ConnectionProvider.getConnection();
+			ResultSet rSet = QueryBuilder.readData(con, "SELECT * FROM users WHERE email='"+email+"'");
+			
+			if (rSet != null) {
+				User newUser = modelHelper.mapResultSetToUser(rSet);
+				user = newUser;
+			}
+			ConnectionProvider.close(con);
 
+		} catch (Exception e) {
+            e.printStackTrace();
+        }
+				
+		return user;
+	}
+	
 }
