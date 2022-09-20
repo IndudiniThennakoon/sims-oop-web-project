6336852c6f472865f5f6a62858328a0713f565fd
Prashantha K.G.M <it21169908@my.sliit.lk>
2022-09-20 18:45:38 +0530

    Internal libraries path change and chage entries

--- src/main/java/com/sims/project/tables/DefaultEntries.java
+++ src/main/java/com/sims/project/tables/DefaultEntries.java
@@ -21,11 +21,11 @@ public class DefaultEntries {
         
         stmt.executeUpdate("INSERT INTO users"
         		+ "(id, name, email, password, nic, profile_photo, user_type, email_verified, email_verify_code, password_reset_code, created_at, updated_at) "
-                + "VALUES(1, 'Admin', 'admin@hims.lk', '12345678', NULL, NULL, 'admin', 0, NULL, NULL, '2022-05-22 23:19:45', '2022-05-23 20:19:45')");
+                + "VALUES(1, 'Admin', 'admin@hims.lk', '12345678', NULL, NULL, '1', 0, NULL, NULL, '2022-05-22 23:19:45', '2022-05-23 20:19:45')");
         
         stmt.executeUpdate("INSERT INTO users"
         		+ "(id, name, email, password, nic, profile_photo, user_type, email_verified, email_verify_code, password_reset_code, created_at, updated_at) "
-                + "VALUES(2, 'Agent', 'agent@hims.lk', '12345678', NULL, NULL, 'agent', 0, NULL, NULL, '2022-05-22 23:20:08', '2022-05-23 20:19:45')");
+                + "VALUES(2, 'Agent', 'agent@hims.lk', '12345678', NULL, NULL, '2', 0, NULL, NULL, '2022-05-22 23:20:08', '2022-05-23 20:19:45')");
 		
 	}
 	
